c9598f3705df3b22fc268822eb1cc7d66c787920
Moses Yeung <mosesyeung@Mosess-MBP.local>
2016-05-10 18:10:42 +0800

    add for redux

--- app/mountView.js
+++ app/mountView.js
@@ -1,5 +1,15 @@
 import routes from './Router';
 import React from 'react';
 import ReactDom from 'react-dom';
-import { Router, browserHistory } from 'react-router'
-ReactDom.render(<Router routes={routes} history={browserHistory}/>,document.getElementById('container'));
+import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoApp from './reducers';
+let store = createStore(todoApp);
+
+ReactDom.render(
+  <Provider store={store}>
+    <Router routes={routes} history={browserHistory}/>
+  </Provider>,
+  document.getElementById('container')
+);
